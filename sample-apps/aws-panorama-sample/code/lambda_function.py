80bbd6f56488f473bd911f3933e72ab2bc63ff51
Michael Wunderlich <mwunderl@amazon.com>
2021-04-21 13:26:16 -0700

    refactor

--- sample-apps/aws-panorama-sample/code/lambda_function.py
+++ sample-apps/aws-panorama-sample/code/lambda_function.py
@@ -18,8 +18,8 @@ class people_counter(panoramasdk.base):
         return {
             "parameters":
                 (
+                    ("model", "model_name", "Name of the model in AWS Panorama", "aws-panorama-sample-model"),
                     ("float", "threshold", "Minimum confidence for display", 0.50),
-                    ("model", "people_counter", "Name of the model in AWS Panorama", "aws-panorama-sample-model"),
                     ("int", "batch_size", "Model batch size", 1),
                     ("float", "person_index", "The index of the person class in the model's dataset", 14),
                 ),
@@ -33,7 +33,6 @@ class people_counter(panoramasdk.base):
                 )
         }
 
-
     def init(self, parameters, inputs, outputs):
         try:
             self.threshold = parameters.threshold
@@ -45,14 +44,13 @@ class people_counter(panoramasdk.base):
             self.frame_time_max = 0
             self.epoch_frames = 150
             self.epoch_start = time.time()
-            self.number_people = 0
             self.colours = np.random.rand(32, 3)
             self.buffered_media = {}
             self.buffered_image = {}
             # Load model
-            logger.info("Loading model: " + parameters.people_counter)
+            logger.info("Loading model: " + parameters.model_name)
             self.model = panoramasdk.model()
-            self.model.open(parameters.people_counter, 1)
+            self.model.open(parameters.model_name, 1)
             os.environ['TVM_TENSORRT_USE_FP16'] = '1'
             # Create input and output arrays
             class_info = self.model.get_output(0)
@@ -109,37 +107,44 @@ class people_counter(panoramasdk.base):
         # While waiting for inference, preprocess the current image
         self.buffered_image[stream] = self.preprocess(media.image)
         self.buffered_media[stream] = media
-        resultBatchSet = self.model.get_result()
+        # Wait for inference results
+        inference_results = self.model.get_result()
+        # Log metrics
         inference_time = (time.time() - inference_start) * 1000
         if inference_time > self.inference_time_max:
             self.inference_time_max = inference_time
         self.inference_time_ms += inference_time
         # Process results
-        #   Model outputs (classes, probabilities, bounding boxes) are collected in 
-        #   the BatchSet returned by model.get_result
-        #   Each output is a Batch of arrays, one for each input in the batch
-        classes = resultBatchSet.get(0)
-        probabilities = resultBatchSet.get(1)
-        boxes = resultBatchSet.get(2)
-        # Each batch only has one image; save results for that image 
+        output = self.process_results(inference_results, output)
+        self.model.release_result(inference_results)
+        return output
+
+    def process_results(self, batch_set, output_media):
+        # Model outputs (classes, probabilities, bounding boxes) are collected in
+        # the BatchSet returned by model.get_result
+        # Each output is a Batch of arrays, one for each input in the batch
+        classes = batch_set.get(0)
+        probabilities = batch_set.get(1)
+        boxes = batch_set.get(2)
+        # Each batch only has one image; save results for that image
         classes.get(0, self.class_array)
         probabilities.get(0, self.prob_array)
         boxes.get(0, self.rect_array)
         # Get indices of people in class array
-        person_indices = self.get_number_persons(self.class_array[0],self.prob_array[0])
-        self.number_people = len(person_indices)
+        person_indices = [i for i in range(len(self.class_array[0])) if int(self.class_array[0][i]) == self.person_index]
+        # Filter out results beneath confidence threshold
+        prob_person_indices = [i for i in person_indices if self.prob_array[0][i] >= self.threshold]
         # Draw bounding boxes on output image
-        for index in person_indices:
+        for index in prob_person_indices:
             left = np.clip(self.rect_array[0][index][0] / np.float(HEIGHT), 0, 1)
             top = np.clip(self.rect_array[0][index][1] / np.float(WIDTH), 0, 1)
             right = np.clip(self.rect_array[0][index][2] / np.float(HEIGHT), 0, 1)
             bottom = np.clip(self.rect_array[0][index][3] / np.float(WIDTH), 0, 1)
-            output.add_rect(left, top, right, bottom)
-            output.add_label(str(self.prob_array[0][index][0]), right, bottom)
+            output_media.add_rect(left, top, right, bottom)
+            output_media.add_label(str(self.prob_array[0][index][0]), right, bottom)
         # Add text
-        output.add_label('People detected: {}'.format(self.number_people), 0.02, 0.9)
-        self.model.release_result(resultBatchSet)
-        return output
+        output_media.add_label('People detected: {}'.format(len(prob_person_indices)), 0.02, 0.9)
+        return output_media
 
     def preprocess(self, img):
         resized = cv2.resize(img, (HEIGHT, WIDTH))
@@ -159,12 +164,6 @@ class people_counter(panoramasdk.base):
         x1[0][1] = img_b
         x1[0][2] = img_c
         return np.asarray(x1)
-    
-    def get_number_persons(self, class_data, prob_data):
-        # Filter out results beneath confidence threshold
-        person_indices = [i for i in range(len(class_data)) if int(class_data[i]) == self.person_index]
-        prob_person_indices = [i for i in person_indices if prob_data[i] >= self.threshold]
-        return prob_person_indices
 
 def main():
     people_counter().run()

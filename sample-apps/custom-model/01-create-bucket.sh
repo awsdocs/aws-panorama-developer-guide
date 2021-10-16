15c8dfd8cefe7d2d46f9fc235d02de61332f11a1
Michael Wunderlich <mwunderl@amazon.com>
2021-10-16 00:18:39 -0700

    create bucket script

--- sample-apps/custom-model/01-create-bucket.sh
+++ sample-apps/custom-model/01-create-bucket.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 SERVICE=aws-panorama
 BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
 BUCKET_NAME=$SERVICE-artifacts-$BUCKET_ID
-echo $BUCKET_NAME > bucket-name.txt
+echo -n $BUCKET_NAME > bucket-name.txt
 aws s3 mb s3://$BUCKET_NAME

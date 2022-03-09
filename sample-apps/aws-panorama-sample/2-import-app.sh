46625dfa8f6d8b180bffaf4463f29b4c76b4f4f5
Michael Wunderlich <mwunderl@amazon.com>
2022-03-09 11:00:49 -0800

    model download script

--- sample-apps/aws-panorama-sample/2-import-app.sh
+++ sample-apps/aws-panorama-sample/2-import-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 # download sample model
-curl https://github.com/awsdocs/aws-panorama-developer-guide/releases/download/v1.0-ga/fd1aef48acc3350a5c2673adacffab06af54c3f14da6fe4a8be24cac687a386e.tar.gz -o assets/fd1aef48acc3350a5c2673adacffab06af54c3f14da6fe4a8be24cac687a386e.tar.gz
+echo "Downloading sample model"
+curl --location --remote-name https://github.com/awsdocs/aws-panorama-developer-guide/releases/download/v1.0-ga/fd1aef48acc3350a5c2673adacffab06af54c3f14da6fe4a8be24cac687a386e.tar.gz -o assets/fd1aef48acc3350a5c2673adacffab06af54c3f14da6fe4a8be24cac687a386e.tar.gz
 # rename directories
 panorama-cli import-application

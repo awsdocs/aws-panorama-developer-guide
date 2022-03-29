3cc20d15d2a980b19873b38f20c4319637f00750
Michael Wunderlich <mwunderl@amazon.com>
2022-03-29 11:11:48 -0700

    cleanup

--- util-scripts/provision-device.sh
+++ util-scripts/provision-device.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 if [[ $# -eq 1 ]] ; then
     DEVICE_NAME=$1
 else
-    echo "Usage: ./register-device.sh <device-name>"
+    echo "Usage: ./provision-device.sh <device-name>"
     exit 1
 fi
 CERTIFICATE_BUNDLE=certificates-omni_${DEVICE_NAME}.zip
